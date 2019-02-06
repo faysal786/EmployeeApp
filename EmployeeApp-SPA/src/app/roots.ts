fba7fe269a871a5e98aa1ebd656707a98ce82a79
faysal786 <6594427+faysal786@users.noreply.github.com>
2019-02-06 16:28:06 +0400

    roots fix for edit resolver

--- EmployeeApp-SPA/src/app/roots.ts
+++ EmployeeApp-SPA/src/app/roots.ts
@@ -8,7 +8,8 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { MemberDetailResolver } from './_resolvers/member-detail.resolvers';
 import { MemberListResolver } from './_resolvers/member-list.resolvers';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
-// import { MemberEditResolver } from './_resolvers/member-edit.resolvers';
+import { MemberEditResolver } from './_resolvers/member-edit.resolvers';
+
 
 
 export const appRoutes: Routes = [
@@ -21,7 +22,7 @@ export const appRoutes: Routes = [
         children: [
             { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver }},
             { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver }},
-            { path: 'member/edit', component: MemberEditComponent},
+            { path: 'member/edit', component: MemberEditComponent, resolve: {user: MemberEditResolver}},
             { path: 'messages', component:  MessagesComponent },
             { path: 'lists', component:  ListsComponent },
         ]
