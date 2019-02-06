f2b02cad90e3dd6bdb4d1c6fa18108f66215bb55
faysal786 <6594427+faysal786@users.noreply.github.com>
2019-02-06 21:52:55 +0400

    Edit changes

--- EmployeeApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ EmployeeApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/_models/user';
 import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -9,12 +10,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberEditComponent implements OnInit {
   user: User;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data['user']; // defined in roots.ts
     });
+  }
+  updateUser() {
+    console.log(this.user);
+    this.alertify.message('Profile update successfully!');
   }
 
 }
